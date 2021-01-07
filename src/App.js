e5195fd42242d6f1d63acdebd7670b26a0d6c507
Stephen Makilan <stephensmakilan@gmail.com>
2021-01-07 23:49:48 +0800

    mapping components

--- src/App.js
+++ src/App.js
@@ -9,31 +9,35 @@ import Joke from './components/Joke';
 
 function App() {
   return (    
-      <div className="contacts">
-            <Joke question="test?" punchLine="One."/>
-            <Joke question="Two?" punchLine="Two."/>
-            <Joke question="Three?" punchLine="Three." />
-            <Joke question="Four?" punchLine="Four." />
-            <Joke question="Five?" punchLine="Five." />
-          {/* <ContactCard 
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/201", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+      <div>
+            <Joke punchLine="It’s hard to explain puns to kleptomaniacs because they always take things literally." />
+            
+            <Joke 
+                question="What's the best thing about Switzerland?" 
+                punchLine="I don't know, but the flag is a big plus!"
             />
             
-            <ContactCard 
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            <Joke 
+                question="Did you hear about the mathematician who's afraid of negative numbers?" 
+                punchLine="He'll stop at nothing to avoid them!"
             />
             
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            <Joke 
+                question="Hear about the new restaurant called Karma?" 
+                punchLine="There’s no menu: You get what you deserve."
             />
             
-            <ContactCard 
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            /> */}
-        {/* <Header />
-        <MainContent />
-        <Footer /> */}
-      </div>
+            <Joke 
+                question="Did you hear about the actor who fell through the floorboards?" 
+                punchLine="He was just going through a stage."
+            />
+            
+            <Joke 
+                question="Did you hear about the claustrophobic astronaut?" 
+                punchLine="He just needed a little space."
+            />
+            
+        </div>
     );
 }
 
