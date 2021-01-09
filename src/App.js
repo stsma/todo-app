a07857fc3fd9bb38f75fe46186c812ba57be56b1
Stephen Makilan <stephensmakilan@gmail.com>
2021-01-09 22:34:46 +0800

    React State Practice

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import React, { Component } from "react";
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            answer: "No"
+// Challenge:
+// Given an incomplete class-based component without a constructor, 
+// add a constructor and initialize state to fix the broken component.
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state={
+            name: "Sally",
+            age: 32
         }
     }
 
     render(){
-        return(
+        return (
             <div>
-                <h1>Is state important to know? {this.state.answer}</h1>
-                <ChildComponent answer={this.state.answer}/>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
             </div>
         )
     }
 }
 
-class ChildComponent extends React.Component{
-    render(){
-        return(
-            <div>
-                {this.props.answer}
-            </div>
-        )
-    }
-}
+// class App extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             answer: "No"
+//         }
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Is state important to know? {this.state.answer}</h1>
+//                 <ChildComponent answer={this.state.answer}/>
+//             </div>
+//         )
+//     }
+// }
+
+// class ChildComponent extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 {this.props.answer}
+//             </div>
+//         )
+//     }
+// }
 
 export default App;
