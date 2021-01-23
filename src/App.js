47463661f936b064c2effdbe02bde3b5f9a81cf8
Stephen Makilan <stephensmakilan@gmail.com>
2021-01-23 23:47:26 +0800

    React Conditional Render Practice Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, {Component} from "react"
-import Conditional from "./components/Conditional"
+import React from "react"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            unreadMessages: ["a", "b"]
-        }
-    }
-    // &&
-    // false && false
-    render() {
-        return (
-            <div>
-                {
-                    this.state.unreadMessages.length > 0 && 
-                    <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-                }
-            </div>
-        )
-    }
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
+
+function App() {
+    return (
+        <div>
+            <button >log in</button>
+        </div>
+    )
 }
 
 export default App
