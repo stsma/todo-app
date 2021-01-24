302547d7dd92150d177f4ff445741d1a502f2431
Stephen Makilan <stephensmakilan@gmail.com>
2021-01-24 23:42:36 +0800

    Fetching data from an API with React part 1

--- src/App.js
+++ src/App.js
@@ -1,55 +1,35 @@
-/**
- * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
- * This challenge is a little more involved than some of the past ones. Check the comments 
- * in the code for some help on accomplishing this one
- * 
- * Challenge: 
- * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
- *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
- * 2. Pass the method down to the TodoItem component
- * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
- */
+import React, {Component} from "react"
 
-import React from "react"
-import todosData from './todosData';
-import TodoItem from './components/ToDoItem';
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
 
-class App extends React.Component {
+class App extends Component {
     constructor() {
         super()
         this.state = {
-            todos: todosData
+            character: {}
         }
-        this.handleChange = this.handleChange.bind(this)
     }
     
-    handleChange(id) {
-        this.setState(prevState => {
-            const updatedTodos = prevState.todos.map(todo => {
-                if (todo.id === id) {
+    componentDidMount() {
+        fetch("https://swapi.dev/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState(d => {
                     return{
-                        ...todo,
-                        completed: !todo.completed
+                        character: data
                     }
-                }
-                return todo;
+                })
             })
-            return {
-                todos: updatedTodos
-            }
-        })
-
-        console.log(this.state.todos);
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-        
         return (
-            <div className="todo-list">
-                {todoItems}
+            <div>
+                Code goes here
             </div>
-        )    
+        )
     }
 }
 
