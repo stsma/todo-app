ca8b110c82039efa2eee82e05a79101b7eb23fda
stephensmakilan <stephensmakilan@gmail.com>
2021-02-07 12:53:46 +0800

    useState() Part 2 - Changing State

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import { render } from "@testing-library/react"
 import React, { useState } from "react"
 
-function App(){
-    const [ answer ] = useState("Yes")
+// Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
+// Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
 
-    return (
+function App(){
+    const [ count, setCount ] = useState(0);
+
+    function increment(){
+        setCount(prevCount => prevCount + 1);
+    }
+
+    function decrement(){
+        setCount(prevCount => prevCount - 1);
+    }
+
+    return(
         <div>
-            <h2>Is State important to know? {answer}</h2>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
 
-// class App extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             answer: "Yes"
-//         }
-//     }
-    
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Is state important to know? {this.state.answer}</h1>
-//             </div>
-//         )
-//     }
-// }
-
-export default App
+export default App;
